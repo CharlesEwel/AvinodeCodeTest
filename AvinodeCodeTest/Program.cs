14f775b0d8bbb30760fbd70a652877fd932bc80b
Charles Ewel <charles.ewel@dealers1.com>
2018-02-15 15:20:35 -0800

    read xml file and print menu display names

--- AvinodeCodeTest/Program.cs
+++ AvinodeCodeTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Text;
+using System.Xml;
 
 namespace AvinodeCodeTest
 {
@@ -8,7 +10,19 @@ namespace AvinodeCodeTest
         static void Main(string[] args)
         {
             string path = "C:/Users/Chuck/Downloads/Wyvern Menu.txt";
-            Console.WriteLine(File.ReadAllText(path));
+            string xmlString = File.ReadAllText(path);
+
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
+            {
+                while(reader.Read())
+                {
+                    reader.ReadToFollowing("displayName");
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        Console.WriteLine(reader.ReadElementContentAsString());
+                    }
+                }
+            }
             Console.Read();
         }
     }
