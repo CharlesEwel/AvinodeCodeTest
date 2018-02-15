1279162f88bb15b3aa579355c5465875ab718bbd
Charles Ewel <charles.ewel@dealers1.com>
2018-02-15 15:29:35 -0800

    gets path and adds that to display

--- AvinodeCodeTest/Program.cs
+++ AvinodeCodeTest/Program.cs
@@ -9,17 +9,27 @@ namespace AvinodeCodeTest
     {
         static void Main(string[] args)
         {
+            //Get XML Data from file and put it into strig
             string path = "C:/Users/Chuck/Downloads/Wyvern Menu.txt";
             string xmlString = File.ReadAllText(path);
 
+            //Initialize reader with which to parse xml string
             using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
             {
                 while(reader.Read())
                 {
+                    //Gets menu name
                     reader.ReadToFollowing("displayName");
+                    //without nnelow if-statement the program hits a bug once it finishes the xml file
                     if (reader.NodeType == XmlNodeType.Element)
                     {
-                        Console.WriteLine(reader.ReadElementContentAsString());
+                        //Gets menu name from inside element
+                        string menuName = reader.ReadElementContentAsString();
+                        //Finds path and gets value of path from attributes list
+                        reader.ReadToFollowing("path");
+                        reader.MoveToFirstAttribute();
+                        string menuPath = reader.Value;
+                        Console.WriteLine(menuName + ", " + menuPath);
                     }
                 }
             }
